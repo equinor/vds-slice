242560c4ad7c83ba02175adc89a91675650cd5ad
Erlend Hårstad <erha@equinor.com>
2022-07-22 14:52:29 +0200

    fixup! Add swagger docs for SliceMetadata

--- api/query.go
+++ api/query.go
@@ -16,7 +16,7 @@ import (
 // Query for slice endpoints
 // @Description Query payload for slice endpoints /slice and /slice/metadata.
 type SliceQuery struct {
-	// The blob path to a vds off form: container/subpath
+	// The blob path to a vds in form: container/subpath
 	Vds string `form:"vds" json:"vds" binding:"required"`
 
 	// Direction can be specified in two domains
