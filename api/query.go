daa0c446ae18e3c7765982c7dd8cdc108b7ff2b7
Erlend Hårstad <erha@equinor.com>
2022-12-22 08:45:26 +0100

    Move request structs to their own file
    query.go contains two distinct parts 1) the request objects and 2) the
    Endpoint handlers. As the file is growing big it makes sense to split
    those parts into two separate files.

--- api/query.go
+++ api/query.go
@@ -11,69 +11,6 @@ import (
 	"github.com/equinor/vds-slice/internal/vds"
 )
 
-type BaseRequest struct {
-	// The blob url to a vds in form
-	// https://account.blob.core.windows.net/container/blob
-	Vds string `json:"vds" binding:"required" example:"https://account.blob.core.windows.net/container/blob"`
-	// A valid sas-token with read access to the container specified in Vds
-	Sas string `json:"sas" binding:"required" example:"sp=r&st=2022-09-12T09:44:17Z&se=2022-09-12T17:44:17Z&spr=https&sv=2021-06-08&sr=c&sig=..."`
-}
-
-type MetadataRequest struct {
-	BaseRequest
-} //@name MetadataRequest
-
-type FenceRequest struct {
-	BaseRequest
-	// Coordinate system for the requested fence
-	// Supported options are:
-	// ilxl : inline, crossline pairs
-	// ij   : Coordinates are given as in 0-indexed system, where the first
-	//        line in each direction is 0 and the last is number-of-lines - 1.
-	// cdp  : Coordinates are given as cdpx/cdpy pairs. In the original SEGY
-	//        this would correspond to the cdpx and cdpy fields in the
-	//        trace-headers after applying the scaling factor.
-	CoordinateSystem string `json:"coordinateSystem" binding:"required" example:"cdp"`
-
-	// A list of (x, y) points in the coordinate system specified in
-	// coordinateSystem, for example [[2000.5, 100.5], [2050, 200], [10, 20]].
-	// All coordinates are expected to be inside the bounding box (with outer margin
-	// of half a distance between consecutive lines).
-	Coordinates [][]float32 `json:"coordinates" binding:"required"`
-
-	// Interpolation method
-	// Supported options are: nearest, linear, cubic, angular and triangular.
-	// Defaults to nearest.
-	// This field is passed on to OpenVDS, which does the actual interpolation.
-	// Note: For nearest interpolation result will snap to the nearest point
-	// as per "half up" rounding. This is different from openvds logic.
-	Interpolation string `json:"interpolation" example:"linear"`
-} //@name FenceRequest
-
-// Query for slice endpoints
-// @Description Query payload for slice endpoint /slice.
-type SliceRequest struct {
-	BaseRequest
-
-	// Direction can be specified in two domains
-	// - Annotation. Valid options: Inline, Crossline and Depth/Time/Sample
-	// - Index. Valid options: i, j and k.
-	//
-	// Only one of Depth, Time and Sample is valid for a given VDS. Which one
-	// depends on the depth units. E.g. Depth is a valid option for a VDS with
-	// depth unit "m" or "ft", but not if the units are "ms", "s".
-	// Sample is valid when the depth is unitless.
-	//
-	// i, j, k are zero-indexed and correspond to Inline, Crossline,
-	// Depth/Time/Sample, respectively.
-	//
-	// All options are case-insensitive.
-	Direction string `json:"direction" binding:"required" example:"inline"`
-
-	// Line number of the slice
-	Lineno *int `json:"lineno" binding:"required" example:"10000"`
-} //@name SliceRequest
-
 type Endpoint struct {
 	MakeVdsConnection vds.ConnectionMaker
 }
