149f35c4b8e4480ebea02acc3942cd00f5ca7623
Alexander Jaust <aej@equinor.com>
2023-01-09 15:40:32 +0100

    Add documentation of bounding box (4/4)

--- internal/vds/boundingbox.h
+++ internal/vds/boundingbox.h
@@ -7,8 +7,14 @@
 #include <OpenVDS/OpenVDS.h>
 #include <OpenVDS/IJKCoordinateTransformer.h>
 
+/// @brief Class that describes the 2D (horizontal) bounding box in
+///        different coordinate systems.
+///
+/// The supported coordinate systems are INDEX, ANNOTATION, and WORLD.
 class BoundingBox {
 public:
+    /// @brief Constructor
+    /// @param layout Pointer to VolumeDataLayout of the open VDS.
     explicit BoundingBox(
         const OpenVDS::VolumeDataLayout *layout
     ) : layout(layout)
@@ -16,11 +22,23 @@ public:
         transformer = OpenVDS::IJKCoordinateTransformer(layout);
     }
 
+    /// @brief Get the bounding box in the index coordinate system.
+    /// @return Vector of four pairs containing the inline, crossline index
+    //          bounding points.
     std::vector< std::pair<int, int> >       index()      noexcept (true);
+    /// @brief Get the bounding box in the annotation coordinate system.
+    /// @return Vector of four pairs containing the annotation coordinates of the
+    ///         bounding points.
     std::vector< std::pair<int, int> >       annotation() noexcept (true);
+    /// @brief Get the bounding box in the world coordinate system.
+    /// @return Vector of four pairs containing the world coordinates of the
+    ///         bounding points.
     std::vector< std::pair<double, double> > world()      noexcept (true);
 private:
+    /// @brief Coordinate transformer that allows thoe conveniently transform
+    ///        between OpenVDS' coordinate systems.
     OpenVDS::IJKCoordinateTransformer transformer;
+    /// @brief Pointer to VolumeDataLayout of open VDS.
     const OpenVDS::VolumeDataLayout*  layout;
 };
 
