3ac1d380001b1831d43dd18aa3d98a2dfd268a15
Erlend Hårstad <erha@equinor.com>
2023-01-09 10:18:51 +0100

    Implement custom gin logger
    This commit introduces a custom request logger for gin. While it's
    currently identical to the default gin logger, it provides a new
    customization point for us.

---
+++ internal/logging/logging.go
@@ -0,0 +1,32 @@
+package logging
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func FormattedLogger() gin.HandlerFunc {
+	return gin.LoggerWithFormatter(func (param gin.LogFormatterParams) string {
+		var statusColor, methodColor, resetColor string
+		if param.IsOutputColor() {
+			statusColor = param.StatusCodeColor()
+			methodColor = param.MethodColor()
+			resetColor  = param.ResetColor()
+		}
+
+		if param.Latency > time.Minute {
+			param.Latency = param.Latency.Truncate(time.Second)
+		}
+		return fmt.Sprintf("[GIN] %v |%s %3d %s| %13v | %15s |%s %-7s %s %#v\n%s",
+			param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+			statusColor, param.StatusCode, resetColor,
+			param.Latency,
+			param.ClientIP,
+			methodColor, param.Method, resetColor,
+			param.Path,
+			param.ErrorMessage,
+		)
+	})
+}
