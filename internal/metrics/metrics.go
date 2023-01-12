fc423b061bdb1ba5933830fdcc8d3b90b6db3c65
Erlend Hårstad <erha@equinor.com>
2023-01-12 13:54:17 +0100

    Add log prometheus histogram for request durations
    This commit implements the metric "vdsslice_durations_histogram_seconds"
    with the following labels "method" (POST/GET), "path"
    (slice/fence/metadata) and "status" (http status).

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -2,13 +2,25 @@ package metrics
 
 
 import (
+	"time"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+const (
+	ms = 0.001
+	s  = 1
+	m  = 60 * s
+)
+
 type metrics struct {
 	registry *prometheus.Registry
+
+	// Custom metics
+	requestDurations *prometheus.HistogramVec
 }
 
 /** Create a new metric instance
@@ -17,12 +29,38 @@ type metrics struct {
  */
 func NewMetrics() *metrics {
 	registry := prometheus.NewRegistry()
-	return &metrics{registry: registry}
+	metrics := &metrics{
+		registry: registry,
+
+		requestDurations: prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name:    "vdsslice_durations_histogram_seconds",
+			Help:    "VDSslice latency distributions.",
+			Buckets: []float64{100*ms, 500*ms, 1*s, 2*s, 5*s, 20*s, 1*m, 2*m},
+		}, []string{"path", "status"}),
+	}
+
+	registry.MustRegister(metrics.requestDurations)
+
+	return metrics;
 }
 
 /** New gin middleware for writing prometheus metrics */
 func NewGinMiddleware(metrics *metrics) gin.HandlerFunc {
-	return func(ctx *gin.Context) { }
+	return func(ctx *gin.Context) {
+		start := time.Now()
+		ctx.Next()
+
+		go func() {
+			path   := ctx.Request.URL.Path
+			status := strconv.Itoa(ctx.Writer.Status())
+
+			duration := time.Since(start).Seconds()
+			metrics.requestDurations.WithLabelValues(
+				path,
+				status,
+			).Observe(duration)
+		}()
+	}
 }
 
 /** New gin handler for prometheus
