472212b36dc61e24cda19799e3d9be4f1af520a0
Alena Chaikouskaya <alec@equinor.com>
2023-01-26 20:17:23 +0100

    Add e2e test for access with cache

--- tests/e2e/test_requests.py
+++ tests/e2e/test_requests.py
@@ -5,7 +5,10 @@ import os
 import pytest
 import json
 import urllib.parse
-from utils.cloud import generate_container_signature
+from utils.cloud import *
+
+from azure.storage import blob
+from azure.storage import filedatalake
 
 from requests_toolbelt.multipart import decoder
 
@@ -136,6 +139,60 @@ def test_assure_no_unauthorized_access(path, payload, sas, allowed_error_message
     error_body = json.loads(res.content)['error']
     assert any([error_msg in error_body for error_msg in allowed_error_messages]), \
         f'error body \'{error_body}\' does not contain any of the valid errors {allowed_error_messages}'
+
+
+@pytest.mark.parametrize("path, payload", [
+    ("slice", make_slice_request(vds=VDSURL)),
+    ("fence", make_fence_request(vds=VDSURL))
+])
+@pytest.mark.parametrize("token, status, error", [
+    (generate_container_signature(
+        STORAGE_ACCOUNT_NAME, CONTAINER, STORAGE_ACCOUNT_KEY,
+        permission=blob.ContainerSasPermissions(read=True)),
+     http.HTTPStatus.OK, None),
+    (generate_account_signature(
+        STORAGE_ACCOUNT_NAME, STORAGE_ACCOUNT_KEY, permission=blob.AccountSasPermissions(
+            read=True), resource_types=blob.ResourceTypes(container=True, object=True)),
+     http.HTTPStatus.OK, None),
+    (generate_blob_signature(
+        STORAGE_ACCOUNT_NAME, CONTAINER, f'{VDS}/VolumeDataLayout', STORAGE_ACCOUNT_KEY,
+        permission=blob.BlobSasPermissions(read=True)),
+     http.HTTPStatus.INTERNAL_SERVER_ERROR, "403 Server failed to authenticate the request"),
+    pytest.param(
+        generate_directory_signature(
+            STORAGE_ACCOUNT_NAME, CONTAINER, VDS, STORAGE_ACCOUNT_KEY,
+            permission=filedatalake.DirectorySasPermissions(read=True)),
+        http.HTTPStatus.OK, None,
+        marks=pytest.mark.skipif(
+            not is_account_datalake_gen2(
+                STORAGE_ACCOUNT_NAME, CONTAINER, VDS, STORAGE_ACCOUNT_KEY),
+            reason="storage account is not a datalake")
+    ),
+])
+# test makes sense if caching is enabled on the endpoint server
+def test_cached_data_access_with_various_sas(path, payload, token, status, error):
+
+    def make_caching_call():
+        container_sas = generate_container_signature(
+            STORAGE_ACCOUNT_NAME,
+            CONTAINER,
+            STORAGE_ACCOUNT_KEY,
+            permission=blob.ContainerSasPermissions(read=True))
+        payload.update({"sas": container_sas})
+        res = requests.post(f'{ENDPOINT}/{path}', json=payload)
+        assert res.status_code == http.HTTPStatus.OK
+
+    make_caching_call()
+
+    payload.update({"sas": token})
+    res = requests.get(
+        f'{ENDPOINT}/{path}',
+        params={"query": json.dumps(payload)}
+    )
+
+    assert res.status_code == status
+    if error:
+        assert error in json.loads(res.content)['error']
 
 
 @pytest.mark.parametrize("path, payload", [
--- tests/utils/cloud.py
+++ tests/utils/cloud.py
@@ -1,17 +1,23 @@
 from azure.storage import blob
+from azure.core import exceptions
 import datetime
+
 
 def generate_account_signature(
     account_name,
     account_key,
     lifespan=120,
+    permission=None,
+    resource_types=None,
 ):
     """
     Create account signature for azure request
     """
     expiry = datetime.datetime.utcnow() + datetime.timedelta(seconds=lifespan)
-    permission = blob.AccountSasPermissions(read=True, list=True)
-    resource_types = blob.ResourceTypes(container=True, object=True)
+    if permission == None:
+        permission = blob.AccountSasPermissions(read=True)
+    if resource_types == None:
+        resource_types = blob.ResourceTypes(container=True, object=True)
 
     return blob.generate_account_sas(
         account_name=account_name,
@@ -19,6 +25,7 @@ def generate_account_signature(
         resource_types=resource_types,
         permission=permission,
         expiry=expiry)
+
 
 def generate_container_signature(
     account_name,
@@ -32,7 +39,7 @@ def generate_container_signature(
     """
     expiry = datetime.datetime.utcnow() + datetime.timedelta(seconds=lifespan)
     if permission == None:
-        permission = blob.ContainerSasPermissions(read=True, list=True)
+        permission = blob.ContainerSasPermissions(read=True)
 
     return blob.generate_container_sas(
         account_name=account_name,
@@ -40,3 +47,79 @@ def generate_container_signature(
         account_key=account_key,
         permission=permission,
         expiry=expiry)
+
+
+def generate_directory_signature(
+    account_name,
+    file_system_name,
+    directory_name,
+    account_key,
+    lifespan=120,
+    permission=None
+):
+    """
+    Create directory signature for azure request
+    """
+    from azure.storage import filedatalake
+    expiry = datetime.datetime.utcnow() + datetime.timedelta(seconds=lifespan)
+    if permission == None:
+        permission = filedatalake.FileSasPermissions(read=True)
+
+    return filedatalake.generate_directory_sas(
+        account_name=account_name,
+        file_system_name=file_system_name,
+        directory_name=directory_name,
+        credential=account_key,
+        permission=permission,
+        expiry=expiry)
+
+
+def generate_blob_signature(
+    account_name,
+    container_name,
+    blob_name,
+    account_key,
+    lifespan=120,
+    permission=None
+):
+    """
+    Create blob signature for azure request
+    """
+    expiry = datetime.datetime.utcnow() + datetime.timedelta(seconds=lifespan)
+    if permission == None:
+        permission = blob.BlobSasPermissions(read=True)
+
+    return blob.generate_blob_sas(
+        account_name=account_name,
+        container_name=container_name,
+        blob_name=blob_name,
+        account_key=account_key,
+        permission=permission,
+        expiry=expiry)
+
+
+def is_account_datalake_gen2(
+    account_name,
+    container_or_filesystem_name,
+    directory_name,
+    account_key,
+):
+    """
+    For the moment datalake gen2 is not our primary storage,
+    but as we aim to support it too, we want to allow
+    additional tests to be run on it
+    """
+
+    from azure.storage import filedatalake
+
+    service_client = filedatalake.DataLakeServiceClient(
+        account_url=f"https://{account_name}.dfs.core.windows.net", credential=account_key)
+    file_system_client = service_client.get_file_system_client(
+        file_system=container_or_filesystem_name)
+    directory_client = file_system_client.get_directory_client(directory_name)
+    try:
+        directory_client.get_directory_properties()
+    except exceptions.ResourceNotFoundError:
+        return False
+    else:
+        return True
