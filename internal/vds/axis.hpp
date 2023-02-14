cd337f4287d2932253ef366aa25430d81d9d57a7
Alexander Jaust <aej@equinor.com>
2023-02-14 11:56:16 +0100

    Add VDS axis wrapper
    The wrapper combines the VDS axis descriptor with the axis' dimension.
    Having this information together is useful for future improvements.

---
+++ internal/vds/axis.hpp
@@ -0,0 +1,32 @@
+#ifndef VDS_SLICE_AXIS_HPP
+#define VDS_SLICE_AXIS_HPP
+
+#include <memory>
+#include <string>
+
+#include <OpenVDS/OpenVDS.h>
+
+#include "vds.h"
+
+class Axis {
+public:
+    Axis(
+        OpenVDS::VolumeDataLayout const * const layout,
+        int const dimension
+    );
+
+    int nsamples() const noexcept(true);
+
+    int min() const noexcept(true);
+    int max() const noexcept(true);
+
+    std::string unit() const noexcept(true);
+    int dimension() const noexcept(true);
+
+    std::string name() const noexcept (true);
+private:
+    int const m_dimension;
+    OpenVDS::VolumeDataAxisDescriptor m_axis_descriptor;
+};
+
+#endif /* VDS_SLICE_AXIS_HPP */
