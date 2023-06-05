7cebd2de3c26efb05d4ea5b61c29f45d6563e7b9
Erlend Hårstad <erha@equinor.com>
2023-06-05 10:36:03 +0200

    Prefer std::generate over raw loops in interpolation
    The standard library's algorithm methods provide superior performance
    over raw loops.

--- internal/vds/interpolation.hpp
+++ internal/vds/interpolation.hpp
@@ -8,6 +8,20 @@
 #include <boost/math/interpolators/makima.hpp>
 
 template< typename T >
+struct StrideGenerator {
+    StrideGenerator(T start, T step) : cur(start), step(step) {}
+
+    T operator()() {
+        T tmp = this->cur;
+        this->cur += this->step;
+        return tmp;
+    }
+private:
+    T cur;
+    T step;
+};
+
+template< typename T >
 void cubic_makima(
     std::vector< T > ys,
     VerticalWindow const& src_window,
@@ -18,24 +32,19 @@ void cubic_makima(
     T nearest = src_window.nearest(reference_point);
     T start = src_window.at(0, nearest);
     T step  = src_window.stepsize();
-    T stop  = start + src_window.size() * step;
 
-    std::vector< T > xs;
-    xs.reserve(ys.size());
-
-    for (int i = start; i < stop; i += step) {
-        xs.push_back(i);
-    }
+    std::vector< T > xs(ys.size());
+    std::generate(xs.begin(), xs.end(), StrideGenerator(start, step));
 
     using boost::math::interpolators::makima;
     auto spline = makima< std::vector< double >>(std::move(xs), std::move(ys));
 
-    T x = dst_window.at(0, reference_point);
+    start = dst_window.at(0, reference_point);
     step  = dst_window.stepsize();
-    for (int i = 0; i < dst_window.size(); i++) {
-        dst_buffer[i] = spline(x);
-        x += step;
-    }
+    auto gen_x = StrideGenerator(start, step);
+    std::generate(dst_buffer.begin(), dst_buffer.end(), [&spline, &gen_x](){
+        return spline(gen_x());
+    });
 }
 
 #endif // VDS_SLICE_INTERPOLATION_HPP
