30a049a365bb11f566f41182a031aa9ffbe2a885
Alena Chaikouskaya <alec@equinor.com>
2023-07-28 18:40:30 +0200

    Extract .cpp file from regularsurface.hpp

--- internal/core/regularsurface.hpp
+++ internal/core/regularsurface.hpp
@@ -2,8 +2,6 @@
 #define VDS_SLICE_REGULAR_SURFACE_HPP
 
 #include <array>
-#include <cmath>
-#include <stdexcept>
 
 struct Point {
     double x;
@@ -15,12 +13,7 @@ struct AffineTransformation : private std::array< std::array< double, 3>, 2 > {
 
     explicit AffineTransformation(base_type x) : base_type(std::move(x)) {}
 
-    Point operator*(Point p) const noexcept (true) {
-        return {
-            this->at(0)[0] * p.x + this->at(0)[1] * p.y + this->at(0)[2],
-            this->at(1)[0] * p.x + this->at(1)[1] * p.y + this->at(1)[2],
-        };
-    };
+    Point operator*(Point p) const noexcept (true);
 
     static AffineTransformation from_rotation(
         float xori,
@@ -28,26 +21,7 @@ struct AffineTransformation : private std::array< std::array< double, 3>, 2 > {
         float xinc,
         float yinc,
         float rot
-    ) noexcept (true) {
-        double rad = rot * (M_PI / 180);
-        /**
-        * Matrix is composed by applying affine transformations [1] in the
-        * following order:
-        * - scaling by xinc, yinc
-        * - counterclockwise rotation by angle rad around the center
-        * - translation by the offset (xori, yori)
-        *
-        * By scaling unit vectors, rotating coordinate system axes and moving
-        * coordinate system center to new position we transform index-based
-        * rows-and-columns cartesian coordinate system into CDP-surface one.
-        *
-        * [1] https://en.wikipedia.org/wiki/Affine_transformation
-        */
-        return AffineTransformation(base_type({{
-            xinc * std::cos(rad),  -yinc * std::sin(rad), xori,
-            xinc * std::sin(rad),   yinc * std::cos(rad), yori
-        }}));
-    }
+    ) noexcept (true);
 };
 
 
@@ -91,31 +65,15 @@ public:
     Point coordinate(
         std::size_t const row,
         std::size_t const col
-    ) const noexcept (false) {
-        if (row >= this->nrows()) throw std::runtime_error("Row out of range");
-        if (col >= this->ncols()) throw std::runtime_error("Col out of range");
-
-        Point point {static_cast<double>(row), static_cast<double>(col)};
-
-        return this->m_transformation * point;
-    }
+    ) const noexcept (false);
 
     /* Value at grid position (row, col) */
     float value(
         std::size_t const row,
         std::size_t const col
-    ) const noexcept (false) {
-        if (row >= this->nrows()) throw std::runtime_error("Row out of range");
-        if (col >= this->ncols()) throw std::runtime_error("Col out of range");
+    ) const noexcept (false);
 
-        return this->m_data[row * this->ncols() + col];
-    };
-
-    float at(std::size_t i) const noexcept (false) {
-        if (i >= this->size()) throw std::runtime_error("index out of range");
-
-        return this->m_data[i];
-    }
+    float at(std::size_t i) const noexcept (false);
     
     float fillvalue() const noexcept (true) { return this->m_fillvalue; };
 
