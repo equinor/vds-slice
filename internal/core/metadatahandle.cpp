9393f7662e3c0026d22c7cde01d301563881719b
Adil Khurshid <akhur@equinor.com>
2023-09-26 13:22:14 +0200

    Get axis order from vds file
    Initially, we assumed that the inline, crossline, and sample dimensions
    in the VDS file would always be in the same order. However, this is not
    always the case, and our code may throw an exception. To address this
    issue, these changes will enable us to retrieve the inline, crossline,
    and sample axis directly from the VDS file, even if they are ordered
    differently in the file.

--- internal/core/metadatahandle.cpp
+++ internal/core/metadatahandle.cpp
@@ -3,7 +3,7 @@
 #include <stdexcept>
 #include <list>
 #include <utility>
-
+#include <boost/algorithm/string/join.hpp>
 #include <OpenVDS/KnownMetadata.h>
 
 #include "axis.hpp"
@@ -12,12 +12,13 @@
 
 MetadataHandle::MetadataHandle(OpenVDS::VolumeDataLayout const * const layout)
     : m_layout(layout),
-      m_iline( Axis(layout, 2)),
-      m_xline( Axis(layout, 1)),
-      m_sample(Axis(layout, 0))
+      m_iline( Axis(layout, get_dimension({std::string(OpenVDS::KnownAxisNames::Inline())}))),
+      m_xline( Axis(layout, get_dimension({std::string(OpenVDS::KnownAxisNames::Crossline())}))),
+      m_sample(Axis(layout, get_dimension({std::string(OpenVDS::KnownAxisNames::Sample()),
+                                           std::string(OpenVDS::KnownAxisNames::Depth()),
+                                           std::string(OpenVDS::KnownAxisNames::Time())})))
 {
     this->dimension_validation();
-    this->axis_order_validation();
 }
 
 void MetadataHandle::dimension_validation() const {
@@ -29,36 +30,16 @@ void MetadataHandle::dimension_validation() const {
     }
 }
 
-/*
- * We're checking our axis order requirement.
- *
- *     voxel[0] -> sample, time or depth
- *     voxel[1] -> crossline
- *     voxel[2] -> inline
- */
-void MetadataHandle::axis_order_validation() const {
-    std::string const msg = "Unsupported axis ordering in VDS, expected "
-        "Sample|Time|Depth, Crossline, Inline";
-
-    using Names = OpenVDS::KnownAxisNames;
-    static const auto SAMPLE = std::string( Names::Sample() );
-    static const auto TIME   = std::string( Names::Time() );
-    static const auto DEPTH  = std::string( Names::Depth() );
-    static const auto ILINE  = std::string( Names::Inline() );
-    static const auto XLINE  = std::string( Names::Crossline() );
-
-    const auto samplename = this->sample().name();
-    if (samplename != SAMPLE and samplename != TIME and samplename != DEPTH) {
-        throw std::runtime_error(msg);
+int MetadataHandle::get_dimension(std::vector<std::string> const & names) const {
+    for (auto i = 0; i < this->m_layout->GetDimensionality(); i++) {
+        std::string dimension_name = this->m_layout->GetDimensionName(i);
+        if (std::find(names.begin(), names.end(), dimension_name) != names.end()) {
+            return i;
+        }
     }
-
-    if (this->xline().name() != XLINE) {
-        throw std::runtime_error(msg);
-    }
-
-    if (this->iline().name() != ILINE) {
-        throw std::runtime_error(msg);
-    }
+    throw std::runtime_error(
+        "Requested axis not found under names " + boost::algorithm::join(names, ", ") +
+        " in vds file ");
 }
 
 Axis MetadataHandle::iline() const noexcept (true) {
