1125400812c44181b2ef10242173f66378be80d6
Yngve Villanger <yvi@equinor.com>
2023-11-21 08:05:08 +0100

    Introduce DataSource

---
+++ internal/core/datasource.hpp
@@ -0,0 +1,44 @@
+#ifndef VDS_SLICE_DATA_SOURCE_HPP
+#define VDS_SLICE_DATA_SOURCE_HPP
+
+#include "datahandle.hpp"
+#include "metadatahandle.hpp"
+
+using voxel = float[OpenVDS::Dimensionality_Max];
+
+/// @brief Generic data source
+class DataSource {
+
+public:
+    virtual ~DataSource() {}
+
+    virtual MetadataHandle const &get_metadata() const noexcept(true) = 0;
+
+    virtual std::int64_t samples_buffer_size(std::size_t const nsamples) noexcept(false) = 0;
+
+    virtual void read_samples(
+        void *const buffer,
+        std::int64_t const size,
+        voxel const *samples,
+        std::size_t const nsamples,
+        enum interpolation_method const interpolation_method) noexcept(false) = 0;
+
+    virtual std::int64_t subcube_buffer_size(SubCube const &subcube) noexcept(false) = 0;
+
+    virtual void read_subcube(
+        void *const buffer,
+        std::int64_t size,
+        SubCube const &subcube) noexcept(false) = 0;
+
+    virtual std::int64_t traces_buffer_size(std::size_t const ntraces) noexcept(false) = 0;
+
+    virtual void read_traces(
+        void *const buffer,
+        std::int64_t const size,
+        voxel const *coordinates,
+        std::size_t const ntraces,
+        enum interpolation_method const interpolation_method) noexcept(false) = 0;
+};
+
+
+#endif /* VDS_SLICE_DATA_SOURCE_HPP */
