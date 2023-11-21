f3f0d5d08223ae5c3965587b5dc83d72808b5d10
Yngve Villanger <yvi@equinor.com>
2023-11-21 10:04:23 +0100

    Add Axis::assert_equal

--- internal/core/axis.cpp
+++ internal/core/axis.cpp
@@ -1,9 +1,11 @@
 #include "axis.hpp"
 
 #include <stdexcept>
+#include "exceptions.hpp"
 
 #include <OpenVDS/IJKCoordinateTransformer.h>
 #include <OpenVDS/KnownMetadata.h>
+#include "utils.hpp"
 
 
 Axis::Axis(
@@ -48,4 +50,59 @@ bool Axis::inrange(float coordinate) const noexcept(true) {
 
 float Axis::to_sample_position(float coordinate) noexcept(false) {
     return this->m_axis_descriptor.CoordinateToSamplePosition(coordinate);
+}
+
+void Axis::assert_equal(Axis const& other) noexcept(false) {
+
+    if (this->nsamples() != other.nsamples()) {
+        throw detail::bad_request(
+            "Axis: " + this->name() +
+            ": Mismatch in number of samples: " +
+            utils::to_string_with_precision(this->nsamples()) +
+            " != " + utils::to_string_with_precision(other.nsamples()));
+    }
+
+    if (this->min() != other.min()) {
+        throw detail::bad_request(
+            "Axis: " + this->name() +
+            ": Mismatch in min value: " +
+            utils::to_string_with_precision(this->min()) +
+            " != " + utils::to_string_with_precision(other.min()));
+    }
+
+    if (this->max() != other.max()) {
+        throw detail::bad_request(
+            "Axis: " + this->name() +
+            ": Mismatch in max value: " +
+            utils::to_string_with_precision(this->max()) +
+            " != " + utils::to_string_with_precision(other.max()));
+    }
+
+    // Stepsize is a data integrity check.
+    // If min,max and nsamples are equal stepsize is equal for consistent data.
+    if (this->stepsize() != other.stepsize()) {
+        throw detail::bad_request(
+            "Axis: " + this->name() +
+            ": Mismatch in stepsize: " +
+            std::to_string(this->stepsize()) +
+            " != " + std::to_string(other.stepsize()));
+    }
+
+    if (this->unit() != other.unit()) {
+        throw detail::bad_request(
+            "Axis: " + this->name() +
+            ": Mismatch in unit: " +
+            this->unit() +
+            " != " + other.unit());
+    }
+
+    // Ignore order of dimensions
+
+    if (this->name() != other.name()) {
+        throw detail::bad_request(
+            "Axis: " + this->name() +
+            ": Mismatch in name: " +
+            this->name() +
+            " != " + other.name());
+    }
 }
