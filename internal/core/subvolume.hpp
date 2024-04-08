33aa3b3962bcaee7b9383d2058fd5a97854e9bbf
Alena Chaikouskaya <alec@equinor.com>
2024-04-08 19:40:11 +0200

    Rename sample_offset variable to zero_sample_offset
    Actually where sample_offset variable is used not any sample offset can
    be accepted. Only sample offset of first sample (which is less than
    stepsize) produces correct calculation. So name is updated.

--- internal/core/subvolume.hpp
+++ internal/core/subvolume.hpp
@@ -50,26 +50,26 @@ public:
     /**
      * Segment size in number of samples
      * It is expected that top_boundary <= bottom_boundary.
-     * sample_offset must be < stepsize.
+     * zero_sample_offset must be < stepsize.
      */
-    std::size_t size(float sample_offset, float top_boundary, float bottom_boundary) const noexcept {
-        return to_below_sample_number(sample_offset, bottom_boundary) - to_above_sample_number(sample_offset, top_boundary) + 1;
+    std::size_t size(float zero_sample_offset, float top_boundary, float bottom_boundary) const noexcept {
+        return to_below_sample_number(zero_sample_offset, bottom_boundary) - to_above_sample_number(zero_sample_offset, top_boundary) + 1;
     }
 
     /**
      * Position of the sample that is closest to the top boundary but is not outside of the segment.
-     * sample_offset must be < stepsize.
+     * zero_sample_offset must be < stepsize.
      */
-    float top_sample_position(float sample_offset, float top_boundary) const noexcept {
-        return to_above_sample_number(sample_offset, top_boundary) * this->stepsize() + sample_offset;
+    float top_sample_position(float zero_sample_offset, float top_boundary) const noexcept {
+        return to_above_sample_number(zero_sample_offset, top_boundary) * this->stepsize() + zero_sample_offset;
     }
 
     /**
      * Position of the sample that is closest to the bottom boundary but is not outside of the segment.
-     * sample_offset must be < stepsize.
+     * zero_sample_offset must be < stepsize.
      */
-    float bottom_sample_position(float sample_offset, float bottom_boundary) const noexcept {
-        return to_below_sample_number(sample_offset, bottom_boundary) * this->stepsize() + sample_offset;
+    float bottom_sample_position(float zero_sample_offset, float bottom_boundary) const noexcept {
+        return to_below_sample_number(zero_sample_offset, bottom_boundary) * this->stepsize() + zero_sample_offset;
     }
 
     float stepsize() const { return m_stepsize; }
@@ -77,18 +77,18 @@ protected:
 
     /**
      * Sequence number of the closest sample that is <= position
-     * sample_offset must be < stepsize.
+     * zero_sample_offset must be < stepsize.
      */
-    int to_below_sample_number(float sample_offset, float position) const noexcept {
-        return floor_with_tolerance((position - sample_offset) / this->stepsize()) + this->margin();
+    int to_below_sample_number(float zero_sample_offset, float position) const noexcept {
+        return floor_with_tolerance((position - zero_sample_offset) / this->stepsize()) + this->margin();
     }
 
     /**
      * Sequence number of the closest sample that is >= position
-     * sample_offset must be < stepsize.
+     * zero_sample_offset must be < stepsize.
      */
-    int to_above_sample_number(float sample_offset, float position) const noexcept {
-        return ceil_with_tolerance((position - sample_offset) / this->stepsize()) - this->margin();
+    int to_above_sample_number(float zero_sample_offset, float position) const noexcept {
+        return ceil_with_tolerance((position - zero_sample_offset) / this->stepsize()) - this->margin();
     }
 
     std::uint8_t margin() const { return m_margin; }
@@ -108,24 +108,24 @@ protected:
 class RawSegmentBlueprint : public SegmentBlueprint {
 public:
     RawSegmentBlueprint(float stepsize, float sample_position) : SegmentBlueprint(stepsize, 2) {
-        m_sample_offset = fmod_with_tolerance(sample_position, this->stepsize());
+        m_zero_sample_offset = fmod_with_tolerance(sample_position, this->stepsize());
     }
 
     std::size_t size(float top_boundary, float bottom_boundary) const noexcept {
-        return SegmentBlueprint::size(m_sample_offset, top_boundary, bottom_boundary);
+        return SegmentBlueprint::size(m_zero_sample_offset, top_boundary, bottom_boundary);
     }
 
     float top_sample_position(float top_boundary) const noexcept {
-        return SegmentBlueprint::top_sample_position(m_sample_offset, top_boundary);
+        return SegmentBlueprint::top_sample_position(m_zero_sample_offset, top_boundary);
     }
 
     float bottom_sample_position(float bottom_boundary) const noexcept {
-        return SegmentBlueprint::bottom_sample_position(m_sample_offset, bottom_boundary);
+        return SegmentBlueprint::bottom_sample_position(m_zero_sample_offset, bottom_boundary);
     }
 
 private:
-    // offset of the first sample from 0, aka m_sample_offset < stepsize
-    float m_sample_offset;
+    // offset of the first sample from 0, i.e. m_zero_sample_offset < stepsize
+    float m_zero_sample_offset;
 };
 
 /**
@@ -140,18 +140,18 @@ public:
     ResampledSegmentBlueprint(float stepsize) : SegmentBlueprint(stepsize, 0) {}
 
     std::size_t size(float reference, float top_boundary, float bottom_boundary) const noexcept {
-        float sample_offset = this->zero_sample_offset(reference);
-        return SegmentBlueprint::size(sample_offset, top_boundary, bottom_boundary);
+        float zero_sample_offset = this->zero_sample_offset(reference);
+        return SegmentBlueprint::size(zero_sample_offset, top_boundary, bottom_boundary);
     }
 
     float top_sample_position(float reference, float top_boundary) const noexcept {
-        float sample_offset = this->zero_sample_offset(reference);
-        return SegmentBlueprint::top_sample_position(sample_offset, top_boundary);
+        float zero_sample_offset = this->zero_sample_offset(reference);
+        return SegmentBlueprint::top_sample_position(zero_sample_offset, top_boundary);
     }
 
     float bottom_sample_position(float reference, float bottom_boundary) const noexcept {
-        float sample_offset = this->zero_sample_offset(reference);
-        return SegmentBlueprint::bottom_sample_position(sample_offset, bottom_boundary);
+        float zero_sample_offset = this->zero_sample_offset(reference);
+        return SegmentBlueprint::bottom_sample_position(zero_sample_offset, bottom_boundary);
     }
 
     /**
@@ -159,9 +159,9 @@ public:
      * It is expected that reference >= top_boundary.
      */
     std::size_t nsamples_above(float reference, float top_boundary) const noexcept {
-        float sample_offset = this->zero_sample_offset(reference);
-        std::size_t top_sample_number = to_above_sample_number(sample_offset, top_boundary);
-        std::size_t reference_sample_number = std::round((reference - sample_offset) / this->stepsize());
+        float zero_sample_offset = this->zero_sample_offset(reference);
+        std::size_t top_sample_number = to_above_sample_number(zero_sample_offset, top_boundary);
+        std::size_t reference_sample_number = std::round((reference - zero_sample_offset) / this->stepsize());
         return reference_sample_number - top_sample_number;
     }
 private:
