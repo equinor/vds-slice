1c46cac5065fff4837bc91803bd6fa2533f90365
Yngve Villanger <yvi@equinor.com>
2024-05-03 14:35:04 +0200

    Introduce DoubleDataHandle

--- internal/core/datahandle.cpp
+++ internal/core/datahandle.cpp
@@ -154,3 +154,173 @@ void SingleDataHandle::read_samples(
         throw std::runtime_error("Failed to read from VDS.");
     }
 }
+
+DoubleDataHandle* make_double_datahandle(
+    const char* url_a,
+    const char* credentials_a,
+    const char* url_b,
+    const char* credentials_b,
+    binary_function binary_operator
+) noexcept(false) {
+    OpenVDS::Error error_a;
+    auto handle_a = OpenVDS::Open(url_a, credentials_a, error_a);
+    if (error_a.code != 0) {
+        throw std::runtime_error("Could not open VDS: " + error_a.string);
+    }
+    OpenVDS::Error error_b;
+    auto handle_b = OpenVDS::Open(url_b, credentials_b, error_b);
+    if (error_b.code != 0) {
+        throw std::runtime_error("Could not open VDS: " + error_b.string);
+    }
+    return new DoubleDataHandle(std::move(handle_a), std::move(handle_b), binary_operator);
+}
+
+DoubleDataHandle::DoubleDataHandle(OpenVDS::VDSHandle handle_a, OpenVDS::VDSHandle handle_b, binary_function binary_operator)
+    : m_file_handle_a(handle_a)
+    , m_file_handle_b(handle_b)
+    , m_binary_operator(binary_operator)
+    , m_access_manager_a(OpenVDS::GetAccessManager(handle_a))
+    , m_access_manager_b(OpenVDS::GetAccessManager(handle_b))
+    , m_metadata_a(m_access_manager_a.GetVolumeDataLayout())
+    , m_metadata_b(m_access_manager_b.GetVolumeDataLayout())
+    , m_metadata(DoubleMetadataHandle(m_metadata_a, m_metadata_b)) 
+    {}
+
+MetadataHandle const& DoubleDataHandle::get_metadata() const noexcept(true) {
+    return this->m_metadata;
+}
+
+OpenVDS::VolumeDataFormat DoubleDataHandle::format() noexcept(true) {
+    /*
+     * We always want to request data in OpenVDS::VolumeDataFormat::Format_R32
+     * format for slice. For fence documentation says: "The traces/samples are
+     * always in 32-bit floating point format."
+     */
+    return OpenVDS::VolumeDataFormat::Format_R32;
+}
+
+std::int64_t DoubleDataHandle::subcube_buffer_size(
+    SubCube const& subcube
+) noexcept(false) {
+    std::int64_t size = this->m_access_manager_a.GetVolumeSubsetBufferSize(
+        subcube.bounds.lower,
+        subcube.bounds.upper,
+        DoubleDataHandle::format(),
+        DoubleDataHandle::lod_level,
+        DoubleDataHandle::channel
+    );
+
+    return size;
+}
+
+/// @brief Shift the provided subcube to match the intersection of the two initial cubes.
+/// @param subcube Subcube contains vectors lower and upper of length 6. Lower defines start index
+/// for each of the 6 dimensions while upper defines end index for the intersection of cube_a and cube_b.
+/// @param metadata Metadata for cube_x (one of the intersecting cubes)
+/// @return Subcube of intersection of cube_a and cube_b in cube_x.
+SubCube DoubleDataHandle::offset_bounds(const SubCube subcube, SingleMetadataHandle metadata_cube_x) {
+
+    // Create a copy
+    SubCube new_subcube = std::move(subcube);
+
+    // Calculate the number of steps to offset the subcube in the three dimensions.
+    float iline_offset = (m_metadata.iline().min() - metadata_cube_x.iline().min()) / m_metadata.iline().stepsize();
+    float xline_offset = (m_metadata.xline().min() - metadata_cube_x.xline().min()) / m_metadata.xline().stepsize();
+    float sample_offset = (m_metadata.sample().min() - metadata_cube_x.sample().min()) / m_metadata.sample().stepsize();
+
+    new_subcube.bounds.lower[m_metadata.iline().dimension()] += iline_offset;
+    new_subcube.bounds.lower[m_metadata.xline().dimension()] += xline_offset;
+    new_subcube.bounds.lower[m_metadata.sample().dimension()] += sample_offset;
+
+    new_subcube.bounds.upper[m_metadata.iline().dimension()] += iline_offset;
+    new_subcube.bounds.upper[m_metadata.xline().dimension()] += xline_offset;
+    new_subcube.bounds.upper[m_metadata.sample().dimension()] += sample_offset;
+    return new_subcube;
+}
+
+void DoubleDataHandle::read_subcube(
+    void* const buffer,
+    std::int64_t size,
+    SubCube const& subcube
+) noexcept(false) {
+    auto request = this->m_access_manager_a.RequestVolumeSubset(
+        buffer,
+        size,
+        OpenVDS::Dimensions_012,
+        DoubleDataHandle::lod_level,
+        DoubleDataHandle::channel,
+        subcube.bounds.lower,
+        subcube.bounds.upper,
+        DoubleDataHandle::format()
+    );
+    bool const success = request.get()->WaitForCompletion();
+
+    if (!success) {
+        throw std::runtime_error("Failed to read from VDS.");
+    }
+}
+
+std::int64_t DoubleDataHandle::traces_buffer_size(std::size_t const ntraces) noexcept(false) {
+    int const dimension = this->get_metadata().sample().dimension();
+    return this->m_access_manager_a.GetVolumeTracesBufferSize(ntraces, dimension);
+}
+
+void DoubleDataHandle::read_traces(
+    void* const buffer,
+    std::int64_t const size,
+    voxel const* coordinates,
+    std::size_t const ntraces,
+    enum interpolation_method const interpolation_method
+) noexcept(false) {
+    int const dimension = this->get_metadata().sample().dimension();
+
+    auto request = this->m_access_manager_a.RequestVolumeTraces(
+        (float*)buffer,
+        size,
+        OpenVDS::Dimensions_012,
+        DoubleDataHandle::lod_level,
+        DoubleDataHandle::channel,
+        coordinates,
+        ntraces,
+        ::to_interpolation(interpolation_method),
+        dimension
+    );
+    bool const success = request.get()->WaitForCompletion();
+
+    if (!success) {
+        throw std::runtime_error("Failed to read from VDS.");
+    }
+}
+
+std::int64_t DoubleDataHandle::samples_buffer_size(
+    std::size_t const nsamples
+) noexcept(false) {
+    return this->m_access_manager_a.GetVolumeSamplesBufferSize(
+        nsamples,
+        DoubleDataHandle::channel
+    );
+}
+
+void DoubleDataHandle::read_samples(
+    void* const buffer,
+    std::int64_t const size,
+    voxel const* samples,
+    std::size_t const nsamples,
+    enum interpolation_method const interpolation_method
+) noexcept(false) {
+    auto request = this->m_access_manager_a.RequestVolumeSamples(
+        (float*)buffer,
+        size,
+        OpenVDS::Dimensions_012,
+        DoubleDataHandle::lod_level,
+        DoubleDataHandle::channel,
+        samples,
+        nsamples,
+        ::to_interpolation(interpolation_method)
+    );
+
+    bool const success = request.get()->WaitForCompletion();
+    if (!success) {
+        throw std::runtime_error("Failed to read from VDS.");
+    }
+}
--- internal/core/datahandle.hpp
+++ internal/core/datahandle.hpp
@@ -4,12 +4,14 @@
 #include <memory>
 #include <string>
 
+#include <functional>
 #include <OpenVDS/OpenVDS.h>
 
 #include "metadatahandle.hpp"
 #include "subcube.hpp"
 
 using voxel = float[OpenVDS::Dimensionality_Max];
+using binary_function = std::function<void(float *, const float *, std::size_t)>;
 
 class SingleDataHandle {
     SingleDataHandle(OpenVDS::VDSHandle handle);
@@ -64,5 +66,69 @@ SingleDataHandle* make_single_datahandle(
     const char* credentials
 ) noexcept (false);
 
+class DoubleDataHandle {
+    DoubleDataHandle(OpenVDS::VDSHandle handle_a, OpenVDS::VDSHandle handle_b, binary_function binary_operator);
+    friend DoubleDataHandle* make_double_datahandle(
+        const char* url_a,
+        const char* credentials_a,
+        const char* url_b,
+        const char* credentials_b,
+        binary_function binary_operator
+    );
+
+public:
+    MetadataHandle const& get_metadata() const noexcept(true);
+
+    static OpenVDS::VolumeDataFormat format() noexcept(true);
+
+    std::int64_t subcube_buffer_size(SubCube const& subcube) noexcept(false);
+
+    void read_subcube(
+        void* const buffer,
+        std::int64_t size,
+        SubCube const& subcube
+    ) noexcept(false);
+
+    std::int64_t traces_buffer_size(std::size_t const ntraces) noexcept(false);
+
+    void read_traces(
+        void* const buffer,
+        std::int64_t const size,
+        voxel const* coordinates,
+        std::size_t const ntraces,
+        enum interpolation_method const interpolation_method
+    ) noexcept(false);
+
+    std::int64_t samples_buffer_size(std::size_t const nsamples) noexcept(false);
+
+    void read_samples(
+        void* const buffer,
+        std::int64_t const size,
+        voxel const* samples,
+        std::size_t const nsamples,
+        enum interpolation_method const interpolation_method
+    ) noexcept(false);
+
+private:
+    OpenVDS::ScopedVDSHandle m_file_handle_a;
+    OpenVDS::ScopedVDSHandle m_file_handle_b;
+    OpenVDS::VolumeDataAccessManager m_access_manager_a;
+    OpenVDS::VolumeDataAccessManager m_access_manager_b;
+    DoubleMetadataHandle m_metadata;
+    SingleMetadataHandle m_metadata_a;
+    SingleMetadataHandle m_metadata_b;
+    binary_function m_binary_operator;
+
+    static int constexpr lod_level = 0;
+    static int constexpr channel = 0;
+
+    SubCube offset_bounds(const SubCube subcube, SingleMetadataHandle metadata);
+};
+
+DoubleDataHandle* make_double_datahandle(
+    const char* url_a, const char* credentials_a,
+    const char* url_b, const char* credentials_b,
+    binary_function binary_operator
+) noexcept(false);
 
 #endif /* VDS_SLICE_DATAHANDLE_HPP */
