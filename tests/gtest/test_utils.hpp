f82657a3d08cd2ef77f5f4fe57d6153a7cdf7929
Alena Chaikouskaya <alec@equinor.com>
2024-07-22 15:17:37 +0200

    Make common class for cube intersection gtests
    gtest files and test methods are now very attuned to cube intersection
    test files. This is unfair as it makes it more difficult to use other
    test files.
    
    Thus moving all cube intersection setup out of test files and into utils
    will separate actual tests and test data setups.
    In addition it should reduce code duplication as same structures appear
    in several different files.

---
+++ tests/gtest/test_utils.hpp
@@ -0,0 +1,57 @@
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+
+#include "cppapi.hpp"
+#include "ctypes.h"
+#include <iostream>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+const std::string CREDENTIALS = "";
+static constexpr float fill = -999.25;
+
+class DatahandleCubeIntersectionTest : public ::testing::Test {
+protected:
+    const std::string REGULAR_DATA = "file://regular_8x2_cube.vds";
+    const std::string SHIFT_4_DATA = "file://shift_4_8x2_cube.vds";
+    const std::string SHIFT_8_32_DATA = "file://shift_8_32x3_cube.vds";
+
+    const int ILINE_STEP = 3;
+    const int XLINE_STEP = 2;
+    const int SAMPLE_STEP = 4;
+
+    DatahandleCubeIntersectionTest()
+        : single_datahandle(make_single_datahandle(REGULAR_DATA.c_str(), CREDENTIALS.c_str())),
+          double_datahandle(make_double_datahandle(
+              REGULAR_DATA.c_str(),
+              CREDENTIALS.c_str(),
+              SHIFT_4_DATA.c_str(),
+              CREDENTIALS.c_str(),
+              binary_operator::ADDITION
+          )),
+          double_reverse_datahandle(make_double_datahandle(
+              SHIFT_4_DATA.c_str(),
+              CREDENTIALS.c_str(),
+              REGULAR_DATA.c_str(),
+              CREDENTIALS.c_str(),
+              binary_operator::ADDITION
+          )),
+          double_different_size(make_double_datahandle(
+              SHIFT_4_DATA.c_str(),
+              CREDENTIALS.c_str(),
+              SHIFT_8_32_DATA.c_str(),
+              CREDENTIALS.c_str(),
+              binary_operator::ADDITION
+          )) {}
+
+public:
+    std::vector<Bound> slice_bounds;
+
+    SingleDataHandle single_datahandle;
+    DoubleDataHandle double_datahandle;
+    DoubleDataHandle double_reverse_datahandle;
+    DoubleDataHandle double_different_size;
+
+};
+#endif // TEST_UTILS_H
